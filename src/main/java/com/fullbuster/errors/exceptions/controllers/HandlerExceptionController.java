89a1103616951c27926144f7f4f54157cf2e9863
Fullbuster <quinthony35@gmail.com>
2025-02-19 09:07:26 -0600

    Feature: HandlerExceptionController add ArithmeticException

--- src/main/java/com/fullbuster/errors/exceptions/controllers/HandlerExceptionController.java
+++ src/main/java/com/fullbuster/errors/exceptions/controllers/HandlerExceptionController.java
@@ -1,17 +1,26 @@
 package com.fullbuster.errors.exceptions.controllers;
 
 import com.fullbuster.errors.exceptions.models.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
 
     @ExceptionHandler({ArithmeticException.class})
-    public ResponseEntity<?> divisionByCero(Exception ex) {
+    public ResponseEntity<Error> divisionByCero(Exception ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error de división por 0");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
-        return ResponseEntity.internalServerError().body("ERROR 500");
+//        return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 
 }
