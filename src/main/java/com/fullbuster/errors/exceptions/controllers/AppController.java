bce2d459147aa6d99c6084568234f55b0f7cb99a
Fullbuster <quinthony35@gmail.com>
2025-02-19 10:04:35 -0600

    Feature: HandlerExceptionController add NumberFormatException

--- src/main/java/com/fullbuster/errors/exceptions/controllers/AppController.java
+++ src/main/java/com/fullbuster/errors/exceptions/controllers/AppController.java
@@ -8,7 +8,10 @@ public class AppController {
 
     @GetMapping("/app")
     public String index() {
-        int value = 100/0;
+//        int value = 100/0;
+
+        int value = Integer.parseInt("10x");
+        System.out.println(value);
         return "OK 200";
     }
 
--- src/main/java/com/fullbuster/errors/exceptions/controllers/HandlerExceptionController.java
+++ src/main/java/com/fullbuster/errors/exceptions/controllers/HandlerExceptionController.java
@@ -4,10 +4,13 @@ import com.fullbuster.errors.exceptions.models.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
@@ -22,6 +25,18 @@ public class HandlerExceptionController {
 
 //        return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler({NumberFormatException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("error", "Número inválido o incorrecto");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return error;
     }
 
     @ExceptionHandler({NoHandlerFoundException.class})
